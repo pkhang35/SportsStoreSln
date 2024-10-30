77c033fd87f8918d4a170f9f5ad276ba367cbd30
pkhang35 <pkhang353@gmail.com>
2024-10-30 08:07:05 +0700

    Add project files.

---
+++ SportsStore/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Components
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        private IStoreRepository repository;
+        public NavigationMenuViewComponent(IStoreRepository repo)
+        {
+            repository = repo;
+        }
+        public IViewComponentResult Invoke()
+        {
+            //Listing 3-10.
+            ViewBag.SelectedCategory =
+            RouteData?.Values["category"];
+            return View(repository.Products
+            .Select(x => x.Category)
+            .Distinct()
+            .OrderBy(x => x));
+        }
+    }
+}
---
+++ SportsStore/Program.cs
@@ -0,0 +1,55 @@
+using Microsoft.EntityFrameworkCore;
+using SportsStore.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<StoreDbContext>(opts => {
+    opts.UseSqlServer(
+        builder.Configuration["ConnectionStrings:SportsStoreConnection"]);
+});
+
+builder.Services.AddScoped<IStoreRepository, EFStoreRepository>();
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.MapControllerRoute("catpage",
+"{category}/Page{productPage:int}",
+new { Controller = "Home", action = "Index" });
+app.MapControllerRoute("page", "Page{productPage:int}",
+new
+{
+    Controller = "Home",
+    action = "Index",
+    productPage = 1
+});
+app.MapControllerRoute("category", "{category}",
+new
+{
+    Controller = "Home",
+    action = "Index",
+    productPage = 1
+});
+app.MapControllerRoute("pagination",
+"Products/Page{productPage}",
+new
+{
+    Controller = "Home",
+    action = "Index",
+    productPage = 1
+});
+/*
+app.MapControllerRoute("pagination",
+"Products/Page{productPage}",
+new { Controller = "Home", action = "Index" });
+*/
+app.MapControllerRoute("pagination",
+    "Products/Page{productPage}",
+    new { Controller = "Home", action = "Index" });
+app.MapDefaultControllerRoute();
+
+SeedData.EnsurePopulated(app);
+
+app.Run();
